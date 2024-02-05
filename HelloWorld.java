598595da370c0ae98876bfe4313ce9df85ce196b
vijay kumar <vijaykumarpalaparthi543@example.com>
2024-02-05 18:42:48 +0530

    Addition functionality

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,12 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World form Bridge Labz2");
+	   
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println("Addition of two values is: "+c);
+		
+	
+	
 	}
-
 }
